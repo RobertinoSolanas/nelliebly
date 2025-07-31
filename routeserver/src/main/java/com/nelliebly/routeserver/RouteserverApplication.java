8e32987b818142b4556353648816aaf4a9fb079f
Robertino Solanas <r.sol@gmx.de>
2025-07-31 17:37:42 +0200

    initiale spring server

---
+++ routeserver/src/main/java/com/nelliebly/routeserver/RouteserverApplication.java
@@ -0,0 +1,13 @@
+package com.nelliebly.routeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouteserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RouteserverApplication.class, args);
+	}
+
+}
---
+++ routeserver/src/test/java/com/nelliebly/routeserver/RouteserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nelliebly.routeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouteserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
